2fefa71a763b0456f1125e11d841ab853bce71e7
もっちゃん <s7056i3056@gmail.com>
2024-09-23 12:05:00 +0900

    feat: オブジェクト指向プログラミングにおけるclassの利用例

--- index.php
+++ index.php
@@ -3,3 +3,38 @@
 // プリミティブな値で「氏名」を表現する
 $full_name = "naruse masanobu";
 echo $full_name . "\n";
+
+// システムによっては姓だけを表示したい
+$tokens = explode(" ", $full_name);
+$last_name = $tokens[0];
+echo $last_name . "\n";
+
+// このロジックではうまく動作しない場合がある
+// smithが姓なのにjohnが表示されてしまう
+$full_name = "john smith";
+$tokens = explode(" ", $full_name);
+$last_name = $tokens[0];
+echo $last_name . "\n";
+
+// このような問題を解決する手段としてオブジェクト指向プログラミングではclassが利用される
+class FullName
+{
+    private string $first_name;
+    private string $last_name;
+
+    public function __construct(string $first_name, string $last_name)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+}
