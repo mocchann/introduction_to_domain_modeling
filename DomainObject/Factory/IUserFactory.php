0a0893375348373d5a2b7fdc46fa29b59a4da32e
もっちゃん <s7056i3056@gmail.com>
2024-10-20 11:43:10 +0900

    add: 必要な引数を追加

--- DomainObject/Factory/IUserFactory.php
+++ DomainObject/Factory/IUserFactory.php
@@ -3,9 +3,10 @@
 namespace DomainObject\Factory;
 
 use DomainObject\Entity\User;
+use DomainObject\ValueObject\UserMailAddress;
 use DomainObject\ValueObject\UserName;
 
 interface IUserFactory
 {
-    public function create(UserName $name): User;
+    public function create(UserName $name, UserMailAddress $mail_address): User;
 }
