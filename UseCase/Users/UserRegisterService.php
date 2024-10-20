54aeead50c7a7ccdb213f0fbfa662b3fcbfa553b
もっちゃん <s7056i3056@gmail.com>
2024-10-20 10:57:44 +0900

    refactor: ユーザー登録処理をfactory経由で実装

--- UseCase/Users/UserRegisterService.php
+++ UseCase/Users/UserRegisterService.php
@@ -3,20 +3,22 @@
 namespace UseCase;
 
 use DomainObject\DomainService\UserService;
-use DomainObject\Entity\User;
+use DomainObject\Factory\IUserFactory;
 use DomainObject\ValueObject\UserMailAddress;
 use DomainObject\ValueObject\UserName;
+use Repository\IUserRepository;
 use Exception;
-use Repositories\IUserRepository;
 use UseCase\Command\UserRegisterCommand;
 
 class UserRegisterService
 {
+    private readonly IUserFactory $user_factory;
     private readonly IUserRepository $user_repository;
     private readonly UserService $user_service;
 
-    public function __construct(IUserRepository $user_repository, UserService $user_service)
+    public function __construct(IUserFactory $user_factory, IUserRepository $user_repository, UserService $user_service)
     {
+        $this->user_factory = $user_factory;
         $this->user_repository = $user_repository;
         $this->user_service = $user_service;
     }
@@ -26,7 +28,7 @@ class UserRegisterService
         $user_name = new UserName($command->getName());
         $mail_address = new UserMailAddress($command->getMailAddress());
 
-        $user = new User($user_name, $mail_address);
+        $user = $this->user_factory->create($user_name, $mail_address);
 
         if ($this->user_service->exists($user)) throw new Exception('User already exists');
 
