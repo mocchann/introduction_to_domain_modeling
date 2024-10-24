632526dab75e58c9a09694210f734abb297bc377
もっちゃん <s7056i3056@gmail.com>
2024-10-24 23:00:00 +0900

    fix: useを修正

--- DomainObject/Factory/Circle/ICircleFactory.php
+++ DomainObject/Factory/Circle/ICircleFactory.php
@@ -3,7 +3,7 @@
 namespace DomainObject\Factory\Circle;
 
 use DomainObject\Entity\Circle\Circle;
-use DomainObject\Entity\User;
+use DomainObject\Entity\Users\User;
 use DomainObject\ValueObject\Circle\CircleName;
 
 interface ICircleFactory
